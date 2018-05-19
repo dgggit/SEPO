a18dfbce5c48a6b2427150c0933258bdb90b7627
Jinho-Ko <ben4945@postech.ac.kr>
2018-05-19 16:48:16 +0900

    login done

--- sepo_server/user_manager/urls.py
+++ sepo_server/user_manager/urls.py
@@ -1,11 +1,12 @@
 #from django.urls import path
 from django.conf.urls import include,url
-from user_manager.views import login#,login_validate
+from user_manager.views import login,login_validate, join
 from . import views
 
 
 urlpatterns = [
 	url(r'^login/$',login),
-	#url(r'^login/validate/$',login_validate),
+	url(r'^login/validate/$',login_validate),
+	url(r'^join/$',join),
 ]
 
--- sepo_server/user_manager/views.py
+++ sepo_server/user_manager/views.py
@@ -6,15 +6,62 @@ from django.template.loader import get_template
 from django.shortcuts import render
 from django.http.response import HttpResponse
 from django.template.context_processors import csrf
-from user_manager.forms import LoginForm
+from user_manager.forms import LoginForm, JoinForm
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def login(req):
 	template = get_template('LoginPage.html')
 
-	context = {}
 	context = {'login_form' : LoginForm()}
-	context.update(csrf(req))
 
-	return HttpResponse(template.render(context))	
+	return render(req,'LoginPage.html',context)
+
+def login_validate(req):
+	
+	login_form_data = LoginForm(req.POST)
+
+	if login_form_data.is_valid():
+		user = authenticate(username = login_form_data.cleaned_data['id'], password = login_form_data.cleaned_data['password'])
+		if user is not None:
+			if user.is_active:
+				auth_login(req,user)
+				return redirect('/community/')
+			
+		else:
+			return HttpResponse('No User or Password Error!')
+	else:
+		return HttpResponse('Abnormal Login Form!')
+
+	return HttpResponse('Unknown Error!')
+
+def join(req):
+	
+	if req.method == 'POST':
+		form_data = JoinForm(req.POST)
+		if form_data.is_valid():
+			usern = form_data.cleaned_data['id']
+			passw = form_data.cleaned_data['password']
+			pwcheck = form_data.cleaned_data['password_check']
+
+			if passw == pwcheck:
+				if not User.objects.filter(username = usern).exists():
+					User.objects.create_user(usern, '', passw)
+					return redirect('/user/login/')
+				else:
+					return HttpResponse('That ID Already Exists!')
+			else:
+				return HttpResponse('Check Password')
+
+	else:
+		form_data = JoinForm()
+				
+	context = {'join_form' : form_data}	
+			
+
+	return render(req,'SignUp.html',context)
+
